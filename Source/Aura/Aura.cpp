e6611c543c185a858e3746ab6893190dca9e3e78
Martin Jakob <martin.jakob@achertreu.ch>
2024-11-24 17:43:32 +0100

    The Base Character Class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Martin Jakob
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Martin Jakob
+
+#pragma once
+
+#include "CoreMinimal.h"
+
