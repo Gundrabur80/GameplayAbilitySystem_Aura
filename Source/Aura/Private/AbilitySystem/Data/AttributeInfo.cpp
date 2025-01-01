4472cebe1d0bfa495bb44ea95eaf5753262a5063
Martin Jakob <martin.jakob@achertreu.ch>
2025-01-01 17:14:44 +0100

    Attribute Info Data Asset

---
+++ Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,23 @@
+// Copyright Martin Jakob
+
+
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+FAuraAttributeInfo UAttributeInfo::FindAuraAttributeInfoForTag(const FGameplayTag& AttribtueTag,
+	bool bLogNotFound) const
+{
+	for (const FAuraAttributeInfo& Info : AttributeInformation)
+	{
+		if (Info.AttributeTag.MatchesTagExact(AttribtueTag))
+		{
+			return Info;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find Info for AttributeTAg [%s] on AttributeInfo [%s]."), *AttribtueTag.ToString(), *GetNameSafe(this));
+	}
+
+	return FAuraAttributeInfo();
+}
