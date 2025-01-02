08af0d6dd363a03d449ece90a5fa3e77a3ec100f
Martin Jakob <martin.jakob@achertreu.ch>
2025-01-02 18:32:35 +0100

    Responding to Attribute Changes

--- Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -30,4 +30,7 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly)
 	TObjectPtr<UAttributeInfo> AttributeInfo;
+
+private:
+	void BroadcastAttributeInfo(const FGameplayTag& AttributeTag, const FGameplayAttribute& Attribute) const;
 };
