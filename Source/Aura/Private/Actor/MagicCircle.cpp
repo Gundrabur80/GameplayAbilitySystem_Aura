eba314f491a207f14e16884624d81cb732475e06
Martin Jakob <martin.jakob@achertreu.ch>
2025-11-22 17:03:26 +0100

    Magic Circle

---
+++ Source/Aura/Private/Actor/MagicCircle.cpp
@@ -0,0 +1,28 @@
+// Copyright Martin Jakob
+
+
+#include "Actor/MagicCircle.h"
+#include "Components/DecalComponent.h"
+
+// Sets default values
+AMagicCircle::AMagicCircle()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	MagicCircleDecal = CreateDefaultSubobject<UDecalComponent>("MagicCircleDecal");
+	MagicCircleDecal->SetupAttachment(RootComponent);
+
+}
+
+void AMagicCircle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMagicCircle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
