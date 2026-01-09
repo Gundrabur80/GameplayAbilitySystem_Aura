30849e2da6774eb23bada9e37d6dad82052a6ec8
Martin Jakob <martin.jakob@achertreu.ch>
2026-01-09 23:26:07 +0100

    Constructing a View Model

--- Source/Aura/Private/UI/HUD/AuraLoadMenuHUD.cpp
+++ Source/Aura/Private/UI/HUD/AuraLoadMenuHUD.cpp
@@ -3,3 +3,16 @@
 
 #include "UI/HUD/AuraLoadMenuHUD.h"
 
+#include "Blueprint/UserWidget.h"
+#include "UI/ViewModel/MVVM_LoadScreen.h"
+#include "UI/Widget/LoadScreenWidget.h"
+
+void AAuraLoadMenuHUD::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	LoadScreenViewModel = NewObject<UMVVM_LoadScreen>(this, LoadScreenViewModelClass);
+	
+	LoadScreedWidget = CreateWidget<ULoadScreenWidget>(GetWorld(), LoadScreedWidgetClass);
+	LoadScreedWidget->AddToViewport();
+}
--- Source/Aura/Public/UI/HUD/AuraLoadMenuHUD.h
+++ Source/Aura/Public/UI/HUD/AuraLoadMenuHUD.h
@@ -6,6 +6,8 @@
 #include "GameFramework/HUD.h"
 #include "AuraLoadMenuHUD.generated.h"
 
+class UMVVM_LoadScreen;
+class ULoadScreenWidget;
 /**
  * 
  */
@@ -13,5 +15,23 @@ UCLASS()
 class AURA_API AAuraLoadMenuHUD : public AHUD
 {
 	GENERATED_BODY()
+
+public:
+	
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> LoadScreedWidgetClass;
+	
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<ULoadScreenWidget> LoadScreedWidget;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UMVVM_LoadScreen> LoadScreenViewModelClass;
+	
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UMVVM_LoadScreen> LoadScreenViewModel;
+	
+protected:
+	
+	virtual void BeginPlay();
 	
 };
