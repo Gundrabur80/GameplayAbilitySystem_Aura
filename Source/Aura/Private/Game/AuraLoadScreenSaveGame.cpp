84a171fedbe68bccfe3f896b3983f72b9c64e5cb
Martin Jakob <martin.jakob@achertreu.ch>
2026-01-17 17:55:39 +0100

    Data Structures for Saving Data

--- Source/Aura/Private/Game/AuraLoadScreenSaveGame.cpp
+++ Source/Aura/Private/Game/AuraLoadScreenSaveGame.cpp
@@ -3,3 +3,26 @@
 
 #include "Game/AuraLoadScreenSaveGame.h"
 
+FSavedMap UAuraLoadScreenSaveGame::GetSavedMapWithName(const FString& InMapName)
+{
+	for (const FSavedMap& Map : SavedMaps)
+	{
+		if (Map.MapAssetName == InMapName)
+		{
+			return Map;
+		}
+	}
+	return FSavedMap();
+}
+
+bool UAuraLoadScreenSaveGame::HasMap(const FString& InMapName)
+{
+	for (const FSavedMap& Map : SavedMaps)
+	{
+		if (Map.MapAssetName == InMapName)
+		{
+			return true;
+		}
+	}
+	return false;
+}
